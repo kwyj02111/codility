2bd0bbc383df00193b6ff0e0c8aea179ef1cc31a
KWON\Administrator <kwyj0211@gmail.com>
2018-11-02 17:49:13 +0900

    [mod] Lesson12 - ChocolatesByNumbers

--- Lesson12/ChocolatesByNumbers.js
+++ Lesson12/ChocolatesByNumbers.js
@@ -1,28 +1,13 @@
 function solution(N, M) {
     // write your code in JavaScript (Node.js 8.9.4)
 
-    let chocoArr = [];
-    let eatCount = 0;
-    let eatNum = 0;
+    return N / gcd(N, M);
+}
 
-    for(let i=0; i<N; i++){
-        chocoArr.push(false);
+function gcd(a, b) {
+    if(a%b === 0){
+        return b;
+    }else{
+        return gcd(b, a%b);
     }
-
-    chocoArr[0] = true;
-    eatCount += 1;
-
-
-    while(N>0){
-        eatNum = (eatNum + M)%N;
-
-        if(!chocoArr[eatNum]){
-            chocoArr[eatNum] = true;
-            eatCount += 1;
-        }else{
-            break
-        }
-    }
-
-    return eatCount;
 }
