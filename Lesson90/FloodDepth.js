97c83dfc6ca13b2f527e0bf196b6fb863d7a4c50
KWON\Administrator <kwyj0211@gmail.com>
2019-01-09 14:16:57 +0900

    [mod] Lesson90 - FloodDepth

--- Lesson90/FloodDepth.js
+++ Lesson90/FloodDepth.js
@@ -7,56 +7,28 @@ function solution(A) {
         return 0;
     }
 
-    let peak = [];
-    for(let i=0; i<len; i++){
+    let highestIdx = 0;
+    let lowestIdx = 0;
+    let max = 0;
 
-        if(i === 0 && A[i] > A[i+1]){
-            peak.push(0);
+    for(let i=1; i<len; i++){
+        let current = A[i];
+        let highest = A[highestIdx];
+        let lowest = A[lowestIdx];
+
+        if(current > highest){
+            max = Math.max(highest - lowest, max);
+            highestIdx = i;
+            lowestIdx = i;
             continue;
         }
 
-        if(i === len-1 && A[i] > A[i-1]){
-            peak.push(i);
-            continue;
-        }
-
-        if(A[i] >= A[i-1] && A[i] >= A[i+1]){
-            peak.push(i);
+        if(current > lowest){
+            max = Math.max(current - lowest, max);
+        }else{
+            lowestIdx = i;
         }
     }
 
-    if(peak.length < 2){
-        return 0;
-    }
-
-    for(let i=1; i<peak.length-1; i++){
-        if(A[peak[i]] < A[peak[i-1]] && A[peak[i]] < A[peak[i+1]]){
-            peak.splice(i,1);
-        }
-    }
-
-    let maxDepth = 0;
-
-    for(let i=0; i<peak.length-1; i++){
-
-        let startIdx = peak[i];
-        let endIdx = peak[i+1];
-        let index = peak[i]+1;
-        let standardPeak = A[startIdx];
-
-        if(A[startIdx] > A[endIdx]){
-            standardPeak = A[endIdx];
-        }
-
-        while(index < endIdx){
-
-            if(standardPeak - A[index] > maxDepth){
-                maxDepth = standardPeak - A[index];
-            }
-
-            index += 1;
-        }
-    }
-
-    return maxDepth;
+    return max;
 }
