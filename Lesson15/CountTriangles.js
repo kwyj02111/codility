aaf9ceb350f036a178dfda48fc8d43818e3e9dd8
KWON\Administrator <kwyj0211@gmail.com>
2019-02-02 17:37:38 +0900

    [add] Lesson15 - CountTriangles

---
+++ Lesson15/CountTriangles.js
@@ -0,0 +1,43 @@
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+
+    let count = 0;
+    let sortA = A.sort(function(a, b) {
+        return a - b;
+    });
+
+    if(sortA.length < 3){
+        return count;
+    }
+
+    for(let i=0; i<sortA.length-2; i++){
+        let first = sortA[i];
+        let middleIdx = i+1;
+        let lastIdx = i+2;
+
+        if(first + sortA[middleIdx] <= sortA[lastIdx]){
+            continue;
+        }
+
+        while(middleIdx < sortA.length-1){
+
+            lastIdx = middleIdx + 1;
+
+            while(lastIdx < sortA.length){
+                let middle = sortA[middleIdx];
+                let last = sortA[lastIdx];
+
+                if(middle + last <= first || first + last <= middle){
+                    break;
+                }
+
+                count += 1;
+                lastIdx += 1;
+            }
+
+            middleIdx += 1;
+        }
+    }
+
+    return count;
+}
